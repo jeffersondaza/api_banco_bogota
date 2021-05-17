aae793b11edf160b0fe0c1ac18b343e77e0a38a2
Jefferson Daza <jefrydaza_206@hotmail.com>
2021-05-17 16:15:59 -0500

    Project init

---
+++ src/main/java/co/edu/usbbog/bdd/ApiBancoBogotaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.bdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoBogotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancoBogotaApplication.class, args);
+	}
+
+}
